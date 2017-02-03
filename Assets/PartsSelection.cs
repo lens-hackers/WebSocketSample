2ceb64c79dac33b7687754ba9dfef93e4fe9e7ef
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2017-02-04 08:48:06 +0900

    first commit

---
+++ Assets/PartsSelection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PartsSelection {
+    public int Ear;
+    public int Nose;
+    public int Brow;
+    public string ImageUrl;
+}
---
+++ Assets/WebSocketComn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+public class WebSocketComn : MonoBehaviour {
+
+    public string IpAddress;
+    public PartsSelection PartsSelection = new PartsSelection();
+    WebSocket ws;
+
+	void Start () {
+        ws = new WebSocket(string.Format("ws://{0}/", IpAddress));
+        ws.OnOpen += (sender, e) => {
+            print("websocket open");
+        };
+        ws.OnError += (sender, e) => {
+            print("WebSocket Error Message: " + e.Message);
+        };
+        ws.OnClose += (sender, e) => {
+            print("WebSocket Close");
+        };
+        ws.OnMessage += Ws_OnMessage;
+        ws.Connect();
+    }
+
+    private void Ws_OnMessage(object sender, MessageEventArgs e) {
+        print(e.Data);
+        JsonUtility.FromJsonOverwrite(e.Data, PartsSelection);
+    }
+
+    void Update () {
+		
+	}
+
+    void OnDestroy() {
+        ws.Close();
+        ws = null;    
+    }
+}

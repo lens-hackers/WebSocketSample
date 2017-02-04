a973a33882364054bf8e7a7e43fcb5ad154e0804
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2017-02-05 06:06:42 +0900

    画像ファイルなど追加

---
+++ Assets/Coordinate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coordinate : MonoBehaviour {
+
+    WebSocketComn webSocketComn;
+    List<string> parts = new List<string>();
+    List<GameObject> displayingParts = new List<GameObject>();
+
+	void Start () {
+        webSocketComn = GetComponent<WebSocketComn>();
+        GameObject prefab = (GameObject)Resources.Load("Prefabs/hige");
+    }
+		
+	void Update () {
+        if (isChanged(webSocketComn.Parts)) {
+            parts = webSocketComn.Parts;
+
+            foreach (var obj in displayingParts) {
+                Destroy(obj);
+            }
+            displayingParts.Clear();
+
+            foreach (var p in parts) {
+                GameObject prefab = (GameObject)Resources.Load("Prefabs/" + p);
+                if (prefab != null) {
+                    var obj = Instantiate(prefab);
+                    displayingParts.Add(obj);
+                }
+            }
+        }
+	}
+
+    bool isChanged(List<string> currentParts) {
+        if (currentParts.Count != parts.Count) {
+            return true;
+        }
+        for (int i = 0; i < currentParts.Count; ++i) {
+            if (currentParts[i] != parts[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
